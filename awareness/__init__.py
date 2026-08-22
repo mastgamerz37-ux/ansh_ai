6f9107fe7fe2fcfa9570fcb9537ed622daf089ef
Anshu <mastgamerz37@gmail.com>
2026-08-22 18:51:06 +0530

    Release v2.0 - Developed by Anshu Dubey

---
+++ awareness/__init__.py
@@ -0,0 +1,5 @@
+"""Awareness engine for monitoring system state, active application, and development context."""
+
+from .engine import AwarenessEngine, AwarenessState
+
+__all__ = ["AwarenessEngine", "AwarenessState"]
